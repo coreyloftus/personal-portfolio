6422e32aedbc26c600144e60c0f3c1da7e23a25e
coreyloftus <coreyloftus@gmail.com>
2023-01-15 08:45:45 -0500

    Added Project 3 to projects section

--- src/Projects.js
+++ src/Projects.js
@@ -6,20 +6,25 @@ const Projects = () => {
             <h3 className="heading-2">Projects</h3>
             <div className="content-container">
                 <div className="project-box">
-                    <h4 className="heading-3">Brewery Buddy</h4>
+                    <h4 className="heading-3">Shoe String</h4>
                     <ul>
                         <li>Created January 2023</li>
                         <li>A full CRUD app using the MERN stack</li>
                         <li>Back End: Express, Mongoose, Node</li>
                         <li>Front End: HTML/CSS, JavaScript, React</li>
                         <li>
-                            <a href="https://brewerybuddy.netlify.app/" target="_blank" rel="noopener noreferrer">
+                            <a href="https://shoe-string.netlify.app/" target="_blank" rel="noopener noreferrer">
                                 Live deployment
                             </a>
                         </li>
                         <li>
-                            <a href="https://github.com/jordbort/project-2-brewery-app" target="_blank" rel="noopener noreferrer">
-                                Github Repo
+                            <a href="https://github.com/coreyloftus/ShoeString-Backend" target="_blank" rel="noopener noreferrer">
+                                Back End Github Repo
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Calathea-Z/ShoeString-Frontend" target="_blank" rel="noopener noreferrer">
+                                Front End Github Repo
                             </a>
                         </li>
                     </ul>

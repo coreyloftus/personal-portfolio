bc7b181968c2b134a0830b3c5d54b0ea2dd837c7
coreyloftus <coreyloftus@gmail.com>
2023-04-11 14:56:24 -0400

    Navbar -> dark mode styling started - social icons stay the same

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,17 +6,17 @@ const Home = () => {
     <div name="home" className="h-screen">
       {/* container */}
       <div className="max-w-[1000px] h-full px-8 flex flex-col justify-center">
-        <p className="text-red-500">Hi there, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#f1e2e2]">
+        <p className="dark:text-red-500">Hi there, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold dark:text-[#f1e2e2]">
           Corey Loftus
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#f29e9e]">
+        <h2 className="text-4xl sm:text-7xl font-bold dark:text-[#f29e9e]">
           I'm a Full Stack Developer.
         </h2>
-        <p className="text-[#f29e9e] py-4 max-w-700">Nice to meet you.</p>
+        <p className="dark:text-[#f29e9e] py-4 max-w-700">Nice to meet you.</p>
         <div>
           <Link to="about" smooth={true} duration={500}>
-            <button className="text-gray-100 rounded-sm group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-500 hover:border-red-500">
+            <button className="dark:text-gray-100 rounded-sm group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-500 hover:border-red-500">
               See what I'm about
               <span className="group-hover:rotate-90 duration-300">
                 <HiArrowNarrowRight className="ml-2" />{" "}

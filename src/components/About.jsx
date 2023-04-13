4c2340ab46a73294fee1b2399a2432ceff78cc1b
coreyloftus <coreyloftus@gmail.com>
2023-04-13 14:36:24 -0400

    Change meta content to remove create-react-app

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,11 @@ const About = () => {
     // About Container
     <div
       name="about"
-      className="w-full min-h-screen text-gray-800 dark:text-gray-100"
+      className={`w-full min-h-screen text-gray-800 dark:text-gray-100`}
     >
-      <div className="flex flex-col justify-center items-center w-full h-screen">
+      <div
+        className={`flex flex-col justify-center items-center w-full h-screen`}
+      >
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-red-500">
--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,49 @@ import Home from "./Home"
 import Navbar from "./Navbar"
 import Skills from "./Skills"
 import Work from "./Work"
+import { useState, useEffect, useRef } from "react"
+import { useInView } from "react-intersection-observer"
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false)
+  const { ref, inView } = useInView({ threshold: 0.5 })
+  const aboutRef = useRef(null)
+  const skillsRef = useRef(null)
+  const workRef = useRef(null)
+  const contactRef = useRef(null)
+  const footerRef = useRef(null)
+  useEffect(() => {
+    if (inView) {
+      setIsVisible(true)
+    }
+  }, [inView])
   return (
     <div className="dark:bg-gray-900 bg-gray-100">
       <div className="dark:bg-red-900/40">
-      <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
-      <Footer />
-    </div>
+        <Navbar />
+        <Home />
+        <About
+          className={`${isVisible ? "fade-up" : "opacity-0"}`}
+          ref={aboutRef}
+        />
+        <Skills
+          className={`${isVisible ? "fade-up" : "opacity-0"}`}
+          ref={skillsRef}
+        />
+        <Work
+          className={`${isVisible ? "fade-up" : "opacity-0"}`}
+          ref={workRef}
+        />
+        <Contact
+          className={`${isVisible ? "fade-up" : "opacity-0"}`}
+          ref={contactRef}
+        />
+        <Footer
+          className={`${isVisible ? "fade-up" : "opacity-0"}`}
+          ref={footerRef}
+        />
       </div>
+    </div>
   )
 }
 

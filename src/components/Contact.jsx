59d76598031bfbd878dc4d968d8c8df83bcad664
coreyloftus <coreyloftus@gmail.com>
2023-04-17 12:54:21 -0400

    Contact -> updating form elements for style (added red ring on focus) and adjusted content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -55,12 +55,12 @@ const Contact = (props) => {
       ref={contactRef}
     >
       <div className="pt-8">
-        <div className="max-w-[1000px] h-full mx-auto flex flex-col justify-center items-center px-4 py-4">
+        <div className="w-full h-full flex flex-col justify-center items-center px-4 py-4">
           <form
             ref={form}
             onSubmit={handleSubmit}
             action=""
-            className="flex flex-col max-w-[600px]"
+            className="flex flex-col"
           >
             <div className="pb-4">
               <div className="flex align-items">
@@ -75,35 +75,34 @@ const Contact = (props) => {
                   <HiArrowNarrowLeft size={40} />
                 </div>
               </div>
-              <p className=" py-4">
-                // Submit the form below to message me, or email me at:{" "}
-                <a href="mailto:coreyloftus@gmail.com">coreyloftus@gmail.com</a>
-                .
+              <p className="py-4">
+                {`// Submit the form below to message me.`}
               </p>
+              <p className="text-sm text-right">{`(Functionality brought to you by EmailJS.)`}</p>
             </div>
             <input
-              className="my-1 p-2 rounded-sm"
+              className="my-1 p-2 rounded-sm w-full focus:outline-none focus:ring-2 focus:ring-red-500"
               type="text"
               placeholder="Name"
               name="Name"
               onChange={handleChange}
             />
             <input
-              className="my-4 p-2 rounded-sm"
+              className="my-4 p-2 rounded-sm w-full focus:outline-none focus:ring-2 focus:ring-red-500"
               type="email"
               placeholder="Email"
               name="name"
               onChange={handleChange}
             />
             <textarea
-              className="my-1 p-2 rounded-sm"
+              className="my-1 p-2 rounded-sm w-full focus:outline-none focus:ring-2 focus:ring-red-500"
               rows="5"
               name="message"
               placeholder="Message"
               onChange={handleChange}
             ></textarea>
-            <button className="rounded-sm border-2 hover:text-gray-100 hover:bg-red-500 hover:border-red-500 px-4 py-3 my-8 mx-auto flex items-center">
-              Send
+            <button className="w-full rounded-sm border-2 hover:text-gray-100 hover:bg-red-500 hover:border-red-500 px-4 py-3 my-8 mx-auto">
+              <span>Send</span>
             </button>
           </form>
         </div>

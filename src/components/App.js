7fd046aa976e72b3de2aaf4751834379dbe66076
coreyloftus <112141387+coreyloftus@users.noreply.github.com>
2023-09-05 10:02:40 -0400

    useLocation to get location for creating hidden /theater and /yoga pages off the main page

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,14 @@ import Home from "./Home"
 import Navbar from "./Navbar"
 import Skills from "./Skills"
 import Work from "./Work"
+import { useLocation } from "react-router-dom"
+
 
 function App() {
+
+const location=useLocation()
+const queryParams = new URLSearchParams(location.search)
+
   return (
     <div className="dark:bg-gray-900 bg-gray-100">
       <div className="dark:bg-red-900/40">
